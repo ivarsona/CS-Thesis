7d95cfa864f52ff5c7bfd6fe786682c12fbc01da
Drew Ivarson <Drew.ivarson@gmail.com>
2015-02-08 21:36:37 -0500

    Added InputParser

---
+++ CFG-Maker/InputParser.java
@@ -0,0 +1,43 @@
+import java.nio.file.*;
+import java.util.*;
+/**
+ * Given the name a file, parses it into an ArrayList of codebytes
+ * 
+ * @author Drew Ivarson
+ * @version 2/8/2015
+ */
+public class InputParser
+{
+    public static ArrayList<CodeByte> parseProgram(String name)
+    {
+        ArrayList<String> fileDump = new ArrayList<String>();
+        Path path = FileSystems.getDefault().getPath(name);
+        try
+        {
+               fileDump = (ArrayList<String>)Files.readAllLines(path);
+               System.out.println(fileDump);
+        }
+        catch (Exception E)
+        {
+               System.out.println("File reader error");
+        }
+        
+        int addrCount = 0;
+        String row = "";
+        ArrayList<CodeByte> program = new ArrayList<CodeByte>();
+        for (int i = 0; i < fileDump.size(); i++)
+        {
+            row = fileDump.get(i);
+            while (!fileDump.get(i).equals(""))
+            {
+                CodeByte current = new CodeByte(addrCount, 
+                            Integer.parseInt(row.substring(0, row.indexOf(" ")), 16));
+                program.add(current);
+                row = row.substring(row.indexOf(" "), row.length());
+                addrCount++;             
+            }
+        }
+        
+        return program;
+    }
+}
